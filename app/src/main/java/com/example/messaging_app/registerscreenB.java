b20badc866209396fa03e55da962a9565f45d352
Jayar <jrsaranza92@gmail.com>
2024-03-22 23:30:18 +0800

    Personal Information

---
+++ app/src/main/java/com/example/messaging_app/registerscreenB.java
@@ -0,0 +1,13 @@
+package com.example.messaging_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class registerscreenB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registerscreen_b);
+    }
+}
